5a85a099c696bfead487d5a49531e98dc2ed8475
Vanessa da Silva Lima de Carvalho <vanessa.carvalho@residencia.dev>
2022-05-29 10:28:28 -0300

    notas alunos

--- 4.notasalunos.py
+++ 4.notasalunos.py
@@ -1,11 +1,14 @@
 import math
 
-qtdNotas = 1
 notas = []
-somaNotas = acimaMedia = somatorioDasDiferenças = 0
+qtdNotas = somaNotas = acimaMedia = somatorioDasDiferenças = 0
 
 while True:
-    nota = float(input('Digite a nota final do aluno {qtdNotas} (-1 para parar) '))
+    nota = float(input(f'Digite a nota final do aluno {qtdNotas+1} (-1 para parar) '))
+
+    while nota > 10:
+        print('O valor informado deve ser entre 0 e 10')
+        nota = float(input(f'Digite a nota final do aluno {qtdNotas+1} (-1 para parar) '))
 
     if (nota < 0):
         break
@@ -14,22 +17,28 @@ while True:
     somaNotas += nota
     qtdNotas += 1
 
-print(f'A quantidade de notas lidas foram: {qtdNotas}')
-print(f'As notas lidas foram: {notas}')
-print(f'A soma das notas é: {somaNotas}')
+if(qtdNotas == 0):
+    print('Nenhuma nota foi informada!')
+    exit()
 
 media = somaNotas / qtdNotas
-print(f'A média das notas é: {media:.2f}')
 
 for nota in notas:
     if (nota > media):
         acimaMedia += 1
         
-print(f'A quantidade de alunos que tiraram nota acima da média foram: {acimaMedia}')
-
 for nota in notas:
     somatorioDasDiferenças += (nota - media)**2
 
 desvioPadrao = math.sqrt(somatorioDasDiferenças / qtdNotas)
 
+print(f'\nA quantidade de notas lidas foram: {qtdNotas}')
+
+print(f'As notas lidas são: ')
+for nota in notas:
+    print(nota, end='; ')
+
+print(f'\nA soma das notas é: {somaNotas}')
+print(f'A média das notas é: {media:.2f}')
+print(f'A quantidade de alunos que tiraram nota acima da média é: {acimaMedia}')
 print(f'O desvio padrão das notas é: {desvioPadrao:.2f}')
